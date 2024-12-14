a4eedee8e4a2e502daab743969536bd48cd21454
deutschbomb <deutschbomb@mail.ru>
2024-12-15 01:02:34 +0300

    Session 0.1

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 
 const createError = require('http-errors');
 const express = require('express');
+const session = require('express-session');
 const fs = require('fs');
 const indexRouter = express.Router();
 const mysql = require('mysql2');
